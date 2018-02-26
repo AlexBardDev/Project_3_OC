1453ee03ec7a480be4827cc27903e18fd2781e6a
AlexandreBardiauxDev <alexandre.bardiaux@outlook.com>
2018-02-26 18:50:12 +0100

    Ajout des images du hero et du gardian. Et premiers essaies de pygame dans game.py

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 #import standard library
 
 #import external library
+import pygame
 
 #import local library
 from characters import Guardian, Hero
@@ -9,4 +10,12 @@ from labyrinth import Labyrinth
 #Generer le laby
 #Generer les perso
 #Placer les objets et les perso
+
+size = 600, 600
+screen = pygame.display.set_mode(size)
+
+hero = pygame.image.load("images/Hero.png")
+screen.blit(hero, (200,300))
+
+pygame.display.flip()
 #While :
