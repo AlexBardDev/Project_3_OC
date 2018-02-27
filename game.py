e28099d3d1582386ccc0f6605e9570fe9119f367
AlexandreBardiauxDev <alexandre.bardiaux@outlook.com>
2018-02-27 18:35:38 +0100

    Création complète du labyrinthe avec le sol et les murs

--- game.py
+++ game.py
@@ -14,19 +14,20 @@ pygame.init()
 #variables
 hero_image_path = "images/Hero.png"
 guardian_image_path = "images/Guardian.png"
-wall_image_path = "images/tiles.png"
+tile_image_path = "images/tile.png"
+wall_image_path = "images/wall.png"
 
 #Generer fenêtre
 size = (600, 600)
 screen = pygame.display.set_mode(size)
 
 #Generer le laby
-board_game = Labyrinth(wall_image_path)
+board_game = Labyrinth(tile_image_path, wall_image_path)
 board_game.generate_labyrinth(screen)
 
 #Generer les perso
 MacGyver = Hero(hero_image_path)
-screen.blit(MacGyver.face, (120,400))
+screen.blit(MacGyver.face, (120,360))
 
 TheGuardian = Guardian(guardian_image_path)
 screen.blit(TheGuardian.face, (280,40))
--- labyrinth.py
+++ labyrinth.py
@@ -12,19 +12,27 @@ class Labyrinth:
 	"""Class from which my labyrinth is derived"""
 
 
-	def __init__(self, image_path):
+	def __init__(self, tile_image_path, wall_image_path):
 		"""My constructor"""
 
-		self.image = image_path
+		self.tile = tile_image_path
+		self.wall = wall_image_path
 
 	def generate_labyrinth(self, screen):
 		"""function that creates the labyrinth"""
 
+		with open("lab.txt", "r") as data :
+			model = data.read()
+
 		j = 0
-		for y in range(15):
+		for line in model.split("\n"):
 			i = 0
-			for x in range(15):
-				square = pygame.image.load(self.image).convert()
+			for letter in line:
+				if letter == "O":
+					image_path = self.wall
+				else:
+					image_path = self.tile
+				square = pygame.image.load(image_path).convert()
 				screen.blit(square, (i,j))
 				i += 40
 			j += 40
