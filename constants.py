2a839cf4f8b3a77cadf4620b7b990be4e6eb232c
AlexandreBardiauxDev <alexandre.bardiaux@outlook.com>
2018-03-01 11:24:46 +0100

    Ajout du fichier constants.py qui contient toutes les constantes du jeu

---
+++ constants.py
@@ -0,0 +1,24 @@
+#Constants of the game
+active = True
+screen_size = (600, 600)
+
+hero_image_path = "images/Hero.png"
+
+guardian_image_path = "images/Guardian.png"
+
+tile_image_path = "images/tile.png"
+wall_image_path = "images/wall.png"
+
+needle_image_path = "images/needle.png"
+plastic_tube_image_path = "images/plastic_tube.png"
+ether_image_path = "images/ether.png"
+
+def initialize_game():
+	"""Function that initialize the game"""
+
+	pass
+
+def game_turn():
+	"""Function called at each turn"""
+
+	pass
--- game.py
+++ game.py
@@ -9,24 +9,10 @@ from pygame.locals import *
 from characters import Guardian, Hero
 from labyrinth import Labyrinth
 from item import Item
+from constants import *
 
 #Initialization of the pygame library
 pygame.init()
-
-#variables
-active = True
-screen_size = (600, 600)
-
-hero_image_path = "images/Hero.png"
-
-guardian_image_path = "images/Guardian.png"
-
-tile_image_path = "images/tile.png"
-wall_image_path = "images/wall.png"
-
-needle_image_path = "images/needle.png"
-plastic_tube_image_path = "images/plastic_tube.png"
-ether_image_path = "images/ether.png"
 
 #Create window
 screen = pygame.display.set_mode(screen_size)
